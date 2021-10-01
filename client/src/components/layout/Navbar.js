dddd7a993590090e1ed9361f600a01e7a4bf7b8b
Don Spire <don.spire1@gmail.com>
2021-09-30 21:39:04 -0700

    add React client

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>This is the main App</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p>This is a full stack React app for uploading images</p>
+      <p>
+        <strong>Version: 1.0.0</strong>
+      </p>
+    </div>
+  );
+};
+
+export default About;
