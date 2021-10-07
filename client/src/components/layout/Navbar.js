13bcd98f16ad651a975c76b9e69bc8c7c770e232
Don Spire <don.spire1@gmail.com>
2021-10-06 18:31:59 -0700

    add Navbar component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useContext, Fragment } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
 
 const Navbar = () => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+
+  const onLogout = () => {
+    logout();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name} </li>
+      <li>
+        <Link to="/profile">Profile Page</Link>
+      </li>
+      <li>
+        <Link to="/upload">Upload Image</Link>
+      </li>
+      <li>
+        <a onClick={onLogout} href="#!">
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div>
       <h1>This is the main App</h1>
@@ -12,6 +49,7 @@ const Navbar = () => {
         <li>
           <Link to="/about">About</Link>
         </li>
+        {isAuthenticated ? authLinks : guestLinks}
       </ul>
     </div>
   );
