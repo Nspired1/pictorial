3353f6f9b8d7be4065cfce56081dc156ff0aa012
Don Spire <don.spire1@gmail.com>
2021-10-07 14:40:43 -0700

    add code for Heroku deploy

--- server.js
+++ server.js
@@ -31,11 +31,19 @@ mongoDB.once("open", () => {
 });
 
 // Routes
-app.get("/", (req, res) => res.json({ msg: "Welcome to Image Uploader" }));
-
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/images", require("./routes/images"));
+
+// serve static assets for production
+if (process.env.NODE_ENV === "production") {
+  //set static folder. React creates a build folder for production
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () =>
   console.log(`Server started and listening on PORT: ${PORT} and IP: ${IP}`)
