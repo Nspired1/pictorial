42667f8efb206a4a4cbf0b3db2270ded9b092341
Don Spire <don.spire1@gmail.com>
2021-10-06 18:32:19 -0700

    add Home component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Images from "../images/Images";
+import ImageFilter from "../images/ImageFilter";
 
 const Home = () => {
   return (
-    <div>
+    <div className="grid-3">
       <h1>Home</h1>
+      <div>
+        {/* ImageForm */}
+        <p>Image Form</p>
+      </div>
+      <div>
+        {/* <ImageFilter /> */}
+        <Images />
+      </div>
     </div>
   );
 };
