055a885ab648b988f5a811c663aa52f485725bea
Don Spire <don.spire1@gmail.com>
2021-10-06 18:31:25 -0700

    add date and user to image

--- models/Image.js
+++ models/Image.js
@@ -3,9 +3,14 @@ const mongoose = require("mongoose");
 const ImageSchema = mongoose.Schema({
   url: String,
   filename: String,
+  description: String,
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "users",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
   },
 });
 
