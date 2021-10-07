bb86fe38b7e3b4ecb2d4c0973f969ed806988333
Don Spire <don.spire1@gmail.com>
2021-10-06 18:28:16 -0700

    minor edits for readability

--- server.js
+++ server.js
@@ -6,28 +6,33 @@ const express = require("express");
 const app = express();
 const mongoDB = require("./models");
 
-// dev dependencies
-const morgan = require("morgan");
-
 // process env or node util
 const PORT = process.env.PORT || 5000;
 const IP = process.env.IP;
 const path = require("path");
 
+// dev dependencies
+const morgan = require("morgan");
+
+// express settings
 app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+// Disabling Powered by tag for security
+app.disable("x-powered-by");
 
 // http request logger, dev setting colorizes output logs
 app.use(morgan("dev"));
 
-// set MongoDB
+// set Mongo Database
 mongoDB.on("error", console.error.bind(console, "Connection ERROR: "));
 mongoDB.once("open", () => {
   console.log("Mongo Atlas Database CONNECTED!");
 });
 
+// Routes
 app.get("/", (req, res) => res.json({ msg: "Welcome to Image Uploader" }));
 
-// Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/images", require("./routes/images"));
