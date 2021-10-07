b4e1fbe6916b2ba53802d33036efe8fbd3a650ec
Don Spire <don.spire1@gmail.com>
2021-10-06 18:33:52 -0700

    update App for Router, Auth, Context

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,50 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import ImageState from "./context/image/ImageState";
+
+import WithPrivacy from "./components/routing/WithPrivacy";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import UploadImage from "./components/images/UploadImage";
+import UserProfile from "./components/pages/UserProfile";
+import Alerts from "./components/layout/Alerts";
+import setAuthToken from "./utils/setAuthToken";
 import "./App.css";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <AuthState>
+      <ImageState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/upload" component={UploadImage} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                  <WithPrivacy exact path="/profile" component={UserProfile} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ImageState>
+    </AuthState>
   );
 }
 
