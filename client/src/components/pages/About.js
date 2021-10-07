d482866d74abac136e99937fda351e4f78bd8818
Don Spire <don.spire1@gmail.com>
2021-10-07 10:53:47 -0700

    minor edits for info

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -4,7 +4,11 @@ const About = () => {
   return (
     <div>
       <h1>About this App</h1>
-      <p>This is a full stack React app for uploading images</p>
+      <p>
+        This is a full stack React app for uploading images. Users can register
+        to create an account, login/logout, upload an image and description, and
+        delete images.
+      </p>
       <p>
         <strong>Version: 1.0.0</strong>
       </p>
